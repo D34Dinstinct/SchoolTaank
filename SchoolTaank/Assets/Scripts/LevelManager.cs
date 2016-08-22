b5fa5fee813d04ce4a372bd53a7843b9c4460290
Benny Coumans <benco.official@hotmail.com>
2016-08-22 17:56:50 +0200

    First Push

---
+++ SchoolTaank/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public Transform spawnPoint, player;
+    private Rigidbody2D playerRig;
+
+	// Use this for initialization
+	void Start () {
+        playerRig = player.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Respawn()
+    {
+        player.position = spawnPoint.position;
+        playerRig.velocity = Vector3.zero;
+    }
+}
---
+++ SchoolTaank/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed, jumpPower, floorCheckRad;
+    public Transform floorCheck;
+    public LayerMask isFloor;
+
+    private bool onFLoor, jumped;
+    private Rigidbody2D player;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator>();
+	}
+	
+    void FixedUpdate()
+    {
+        onFLoor = Physics2D.OverlapCircle(floorCheck.position, floorCheckRad, isFloor);
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (onFLoor) {
+            jumped = false;
+            anim.SetBool("isJumping", false);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            player.velocity = new Vector2(moveSpeed, player.velocity.y);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.velocity = new Vector2(-moveSpeed, player.velocity.y);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && onFLoor)
+        {
+            Jump();
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) && !jumped && !onFLoor)
+        {
+            Jump();
+            jumped = true;
+        }
+        anim.SetFloat("speed", Mathf.Abs(player.velocity.x));
+    }
+
+    public void Jump()
+    {
+        player.velocity = new Vector2(player.velocity.x, jumpPower);
+        anim.SetBool("isJumping", true);
+    }
+}
